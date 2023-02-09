b9259136b075abb9b0c643dbe71831ec3b845b6f
Th3WarPheonix <113734956+Th3WarPheonix@users.noreply.github.com>
2023-02-08 21:24:49 -0500

    added combustor and multiple configurations

--- Modular_GTE_Calcs.py
+++ Modular_GTE_Calcs.py
@@ -30,6 +30,15 @@ def compressor(total_temperature:float, total_pressure:float, efficiency:float, 
 
     return total_temperature2_ideal, total_temperature2_actual, total_pressure2, ideal_work, actual_work
 
+def combustor(Tt31, Tt4, m31, LHV, comb_eff, gamma_hot, gamma_cold=1.4, R_air=287.05):
+    cp_cold = gamma_cold*R_air/(gamma_cold-1)
+    cp_hot = gamma_hot*R_air/(gamma_hot-1)
+    
+    mfuel = (m31*cp_cold*Tt31 - m31*cp_hot*Tt4)/(cp_hot*Tt4 - LHV)
+    fuel_air_ratio = (cp_hot*Tt4 - cp_cold*Tt31)/(LHV*comb_eff - cp_hot*Tt4)
+    
+    return fuel_air_ratio
+
 def inlet_design(stream_density:float, stream_velocity:float, massflow:float, A0AHL:float, mach_throat:float, Tt0:float, Pt0:float, Ts1:float, Ps1:float, mach_fan:float, diffuser_angle:float, gamma:float=1.4, R_air:float=287.05):
 
     '''Entrance calcs'''
@@ -63,29 +72,83 @@ def inlet_design(stream_density:float, stream_velocity:float, massflow:float, A0
 
     return np.array([streamtube_diameter, highlight_diameter, throat_diameter, fan_diameter]), diffuser_length, diffuser_length/throat_diameter
 
-def convert_temps(temps):
+def engine_configuration(Tambient, Pambient, mach0, mach1, inlet_press_rec, fan_eff, fan_press_ratio, bypass_ratio, comp_eff, comp_press_ratio, m31, LHV, Tt4, comb_eff, gamma_hot):
+    Tt0, Pt0 = ambient_properties(mach0, Tambient, Pambient)
+    Tt1, Pt1, Ts1, Ps1, Vel1 = inlet(mach1, Tt0, Pt0, inlet_press_rec)
+    Tt13i, Tt13a, Pt13, Wfi, Wfa = compressor(Tt1, Pt1, fan_eff, fan_press_ratio, bypass_ratio) # for the fan
+    Tt3i, Tt3a, Pt3, Wci, Wca = compressor(Tt13a, Pt13, comp_eff, comp_press_ratio, 0)
+    Tt31 = Tt13a
+    fuel_air_ratio = combustor(Tt31, Tt4, m31, LHV, comb_eff, gamma_hot)
+
+    temperautres_emps = convert_temps([Tambient, Tt0, Tt1, Ts1, Tt13i, Tt13a, Tt3i, Tt3a], 'emp')
+    Ts0_emp, Tt0_emp, Tt1_emp, Ts1_emp, Tt13i_emp, Tt13a_emp, Tt3i_emp, Tt3a_emp = temperautres_emps
+
+    pressures_emps = convert_pressures([Pambient, Pt0, Pt1, Ps1, Pt13, Pt3], 'emp')
+    Ps0_emp, Pt0_emp, Pt1_emp, Ps1_emp, Pt13_emp, Pt3_emp = pressures_emps
+
+    Vel1_emp = Vel1 / 12 / .0254
+
+    works_emps = convert_work([Wfi, Wfa, Wci, Wca], 'emp')
+    Wfi_emp, Wfa_emp, Wci_emp, Wca_emp = works_emps
+
+    labels0 = np.array(['Total Temperature (R)', 'Static Temperature (R)', 'Total Pressure (psia)', 'Static Pressure (psia)', 'Mach'])
+    station0 = pd.Series([Tt0_emp, Ts0_emp, Pt0_emp, Ps0_emp, mach0], index=[np.repeat('0 Freestream', len(labels0)), labels0])
+
+    labels1 = np.array(['Total Temperature (R)', 'Static Temperature (R)', 'Total Pressure (psia)', 'Static Pressure (psia)', 'Mach', 'Velocity (ft/s)'])
+    station1 = pd.Series([Tt1_emp, Ts1_emp, Pt1_emp, Ps1_emp, mach1, Vel1_emp], index=[np.repeat('1 Fan Inlet', len(labels1)), labels1])
+
+    labels13 = np.array(['Total Temperature Ideal (R)', 'Total Temperature Actual (R)', 'Total Pressure (psia)', 'Work Ideal (BTU/lbm)', 'Work Actual (BTU/lbm)'])
+    station13 = pd.Series([Tt13i_emp, Tt13a_emp, Pt13_emp, Wfi_emp, Wfa_emp], index=[np.repeat('13 Bypass/Compressor Inlet', len(labels13)), labels13])
+
+    labels3 = np.array(['Total Temperature Ideal (R)', 'Total Temperature Actual (R)', 'Total Pressure (psia)', 'Work Ideal (BTU/lbm)', 'Work Actual (BTU/lbm)'])
+    station3 = pd.Series([Tt3i_emp, Tt3a_emp, Pt3_emp, Wci_emp, Wca_emp], index=[np.repeat('3 Comubstor Inlet', len(labels3)), labels3])
+
+    return station0, station1, station13, station3
+
+def convert_temps(temps, to):
     """Convert the temperature from K to R"""
-    try:
-        temps *= 1.8
-        return np.array([temps])
-    except:
-        return np.array(temps)*1.8
+    if to == 'emp':
+        try:
+            temps *= 1.8
+            return temps
+        except:
+            return np.array(temps)*1.8
+    elif to == 'SI':
+        try:
+            temps = temps/1.8
+            return temps
+        except:
+            return np.array(temps)/1.8
 
-def convert_pressures(pressures):
+def convert_pressures(pressures, to):
     """Convert the pressure from Pa to psia"""
-    try:
-        pressures *= 1.8
-        return np.array([pressures])
-    except:
-        return np.array(pressures)/6895
+    if to == 'emp':
+        try:
+            pressures = pressures/6895
+            return pressures
+        except:
+            return np.array(pressures)/6895
+    elif to == 'SI':
+        try:
+            pressures = pressures*6895
+            return pressures
+        except:
+            return np.array(pressures)* 6895
 
-def convert_work(works):
+def convert_work(works, to):
     """Convert the work from J to Btu"""
-    try:
-        works *= 1.8
-        return np.array([works])
-    except:
-        return np.array(works) / 1055 / 2.205
+    if to == 'emp':
+        try:
+            works = works / 1055 / 2.205
+            return works
+        except:
+            return np.array(works) / 1055 / 2.205
+    elif to == 'SI':
+        try:
+            works = works * 1055 * 2.205
+            return works
+        except:
+            return np.array(works) * 1055 * 2.205
 
 def main():
     Rair = 287.05 # J/kg-K
@@ -104,39 +167,33 @@ def main():
     fan_press_ratio = 1.6
     
     comp_eff = .87
-    comp_press_ratio = 7
+    comp_press_ratio = [7, 9, 9]
+
+    m31 = .9
+    LHV = 18550 # BTU/lbmfuel
+    LHV = convert_work(LHV, 'SI')
+    turbine_inlet_temp = [2100, 2100, 2200] # F
+    Tt4 = convert_temps(turbine_inlet_temp, 'SI')
+    comb_eff = .995
+    gamma_hot = 1.3
     
-    Tt0, Pt0 = ambient_properties(mach0, Tambient, Pambient)
-    Tt1, Pt1, Ts1, Ps1, Vel1 = inlet(mach1, Tt0, Pt0, inlet_press_rec)
-    Tt13i, Tt13a, Pt13, Wfi, Wfa = compressor(Tt1, Pt1, fan_eff, fan_press_ratio, bypass_ratio) # for the fan
-    Tt2i, Tt2a, Pt2, Wci, Wca = compressor(Tt13a, Pt13, comp_eff, comp_press_ratio, 0)
-
-    temperautres_emps = convert_temps([Tambient, Tt0, Tt1, Ts1, Tt13i, Tt13a, Tt2i, Tt2a])
-    Ts0_emp, Tt0_emp, Tt1_emp, Ts1_emp, Tt13i_emp, Tt13a_emp, Tt3i_emp, Tt3a_emp = temperautres_emps
-
-    pressures_emps = convert_pressures([Pambient, Pt0, Pt1, Ps1, Pt13, Pt2])
-    Ps0_emp, Pt0_emp, Pt1_emp, Ps1_emp, Pt13_emp, Pt2_emp = pressures_emps
-
-    Vel1_emp = Vel1 / 12 / .0254
-
-    works_emps = convert_work([Wfi, Wfa, Wci, Wca])
-    Wfi_emp, Wfa_emp, Wci_emp, Wca_emp = works_emps
-
-    labels0 = np.array(['Total Temperature (R)', 'Static Temperature (R)', 'Total Pressure (psia)', 'Static Pressure (psia)', 'Mach'])
-    station0 = pd.Series([Tt0_emp, Ts0_emp, Pt0_emp, Ps0_emp, mach0], index=[np.repeat('0 Freestream', len(labels0)), labels0])
-
-    labels1 = np.array(['Total Temperature (R)', 'Static Temperature (R)', 'Total Pressure (psia)', 'Static Pressure (psia)', 'Mach', 'Velocity (ft/s)'])
-    station1 = pd.Series([Tt1_emp, Ts1_emp, Pt1_emp, Ps1_emp, mach1, Vel1_emp], index=[np.repeat('1 Fan Inlet', len(labels1)), labels1])
-
-    labels13 = np.array(['Total Temperature Ideal (R)', 'Total Temperature Actual (R)', 'Total Pressure (psia)', 'Work Ideal (BTU/lbm)', 'Work Actual (BTU/lbm)'])
-    station13 = pd.Series([Tt13i_emp, Tt13a_emp, Pt13_emp, Wfi_emp, Wfa_emp], index=[np.repeat('13 Bypass Inlet', len(labels13)), labels13])
-
-    labels2 = np.array(['Total Temperature Ideal (R)', 'Total Temperature Actual (R)', 'Total Pressure (psia)', 'Work Ideal (BTU/lbm)', 'Work Actual (BTU/lbm)'])
-    station2 = pd.Series([Tt3i_emp, Tt3a_emp, Pt2_emp, Wci_emp, Wca_emp], index=[np.repeat('2 Compressor Inlet', len(labels2)), labels2])
-    
-    dfConfigs = pd.DataFrame(pd.concat([station0, station1, station13, station2]), columns=['Config1'])
+    dfConfigs = pd.DataFrame()
+    for i in range(3):
+        station0, station1, station13, station3 = engine_configuration(Tambient, Pambient, mach0, mach1, inlet_press_rec, fan_eff, fan_press_ratio, bypass_ratio, comp_eff, comp_press_ratio[i], m31, LHV, Tt4[i], comb_eff, gamma_hot)
+        dfConfigi = pd.DataFrame(pd.concat([station0, station1, station13, station3]), columns=['Config {}'.format(i+1)])
+        dfConfigs = pd.concat([dfConfigs, dfConfigi], axis=1)
     dfConfigs.index=dfConfigs.index.rename(['Station','Property'])
     print(dfConfigs)
+
+    plt.plot(comp_press_ratio[0:2], dfConfigs['Config 1'].loc['3 Combustor', 'Total Temperature Actual (R)']
+    
+    )
+    Tt0 = convert_temps(dfConfigs['Config 1'].loc['0 Freestream', 'Total Temperature (R)'], 'SI')
+    Pt0 = convert_pressures(dfConfigs['Config 1'].loc['0 Freestream', 'Total Pressure (psia)'], 'SI')
+    Ts1 = convert_temps(dfConfigs['Config 1'].loc['1 Fan Inlet', 'Static Temperature (R)'], 'SI')
+    Ps1 = convert_pressures(dfConfigs['Config 1'].loc['1 Fan Inlet', 'Static Pressure (psia)'], 'SI')
+
+    '''Component Design'''
     density0 = Pambient/(Rair*Tambient)
     velocity0 = 795.5 # ft/s
     velocity0 = velocity0 * 12*.0254
@@ -147,7 +204,8 @@ def main():
     fan_mach = .4
     diffuser_angle = 5
     diams, diff_length, diff_LH = inlet_design(density0, velocity0, massflow0, A0Ahl, throat_mach, Tt0, Pt0, Ts1, Ps1, fan_mach, diffuser_angle)
-    print(diams/.0254, diff_length/.0254, diff_LH)
+    # print(diams/.0254, diff_length/.0254, diff_LH)
 
 if __name__ == '__main__':
     main()
+    
